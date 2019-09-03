c38c753f17807730e81d79d64efe59c0caca33a2
Stephen Parker <stephenparkhum@outlook.com>
2019-09-03 13:16:23 -0700

    Updating with test endpoint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
  const express = require('express');
+ const morgan = require('morgan');
  const app = express();
+ const cors = require('cors');
+ const {
+   CLIENT_ORIGIN
+ } = require('./config');
+
+ app.use(
+   cors({
+     origin: CLIENT_ORIGIN
+   })
+ );
 
  const PORT = process.env.PORT || 3000;
+ const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+ app.use(morgan(morganSetting))
+
 
  app.get('/api/*', (req, res) => {
-   res.json({ok: true});
+   res.send('Your API route is working!');
+ });
+
+ app.get('/', (req, res) => {
+   res.send('Your server is working!');
  });
 
  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
- module.exports = {app};
+ module.exports = {
+   app
+ };
