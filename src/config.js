f384abeaa5124497cdfe9bdd5d85ecefca7e04f9
Stephen Parker <stephenparkhum@outlook.com>
2019-09-03 22:47:36 -0700

    fixing error

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "https://peaceful-fortress-94581.herokuapp.com/api",
-    CLIENT_ORIGIN: 'http://localhost:3000/'
+    CLIENT_ORIGIN: 'http://localhost:3000'
 };
