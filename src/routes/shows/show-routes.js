78617caee134d8c62510b3f0385aa53b1e6cd16b
Stephen Parker <stephenparkhum@outlook.com>
2019-09-17 20:00:40 -0700

    Updating showslist

--- src/routes/shows/show-routes.js
+++ src/routes/shows/show-routes.js
@@ -1,9 +1,62 @@
 const express = require('express');
 const shows = require('express').Router();
-const showList = require('./shows-test.json');
+// const showList = require('./shows-test.json');
 const jsonParser = express.json();
 const xss = require('xss');
 
+const showList = [
+    {
+        "id": 1,
+        "artist_id": 1,
+        "date": "1/1/2020",
+        "location": "Portland, OR",
+        "venue": "Roseland Theater",
+        "support_bands": ["Velnias", "Willowbrook"],
+        "fb_event": "http://facebook.com",
+        "tour_id": 1
+    },
+    {
+        "id": 2,
+        "artist_id": 1,
+        "date": "1/2/2020",
+        "location": "Eugene, OR",
+        "venue": "The WOW Hall",
+        "support_bands": ["Velnias", "Willowbrook"],
+        "fb_event": "http://facebook.com",
+        "tour_id": 1
+    },
+    {
+        "id": 3,
+        "artist_id": 1,
+        "date": "1/3/2020",
+        "location": "Salem, OR",
+        "venue": "Graveyard Bar",
+        "support_bands": ["Velnias", "Willowbrook"],
+        "fb_event": "http://facebook.com",
+        "tour_id": 2
+    },
+    {
+        "id": 4,
+        "artist_id": 1,
+        "date": "1/4/2020",
+        "location": "Seattle, WA",
+        "venue": "Highline",
+        "support_bands": ["Velnias", "Willowbrook"],
+        "fb_event": "http://facebook.com",
+        "tour_id": 2
+    },
+    {
+        "id": 5,
+        "artist_id": 1,
+        "date": "1/5/2020",
+        "location": "Spokane, WA",
+        "venue": "Fake Venue",
+        "support_bands": ["Velnias", "Willowbrook", "Jesus Band"],
+        "fb_event": "http://facebook.com",
+        "tour_id": 2
+    }
+]
+;
 
 shows
     .get('/', (req, res) => {
